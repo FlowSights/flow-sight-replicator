32b512a3a0250453d16f036a89d854b1db1db5d6
FlowSights <contacto@flowsights.it.com>
2026-05-03 01:22:38 -0600

    feat: Apple Pro redesign and middleware fix

--- middleware.ts
+++ middleware.ts
@@ -43,8 +43,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)
-  ',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
 // Trigger new commit
